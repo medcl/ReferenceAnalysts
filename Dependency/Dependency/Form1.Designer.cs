a348cdbaa10e85f1e1d16f9e4dae6996f10630cd
Medcl <medcl@qq.com>
2011-05-09 22:51:59 +0800

    initial commit

---
+++ Dependency/Dependency/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Dependency
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.comboBox1 = new System.Windows.Forms.ToolStripComboBox();
+			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.panel1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.statusStrip1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(595, 388);
+			this.panel1.TabIndex = 3;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.comboBox1,
+            this.toolStripButton2});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(855, 25);
+			this.toolStrip1.TabIndex = 4;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(107, 22);
+			this.toolStripButton1.Text = "Where are they?";
+			this.toolStripButton1.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownWidth = 200;
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(400, 25);
+			// 
+			// toolStripButton2
+			// 
+			this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+			this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton2.Name = "toolStripButton2";
+			this.toolStripButton2.Size = new System.Drawing.Size(56, 22);
+			this.toolStripButton2.Text = "Analyze";
+			this.toolStripButton2.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.panel1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+			this.splitContainer1.Size = new System.Drawing.Size(855, 388);
+			this.splitContainer1.SplitterDistance = 595;
+			this.splitContainer1.TabIndex = 5;
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer2.Name = "splitContainer2";
+			this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.Controls.Add(this.richTextBox2);
+			this.splitContainer2.Panel1.Controls.Add(this.panel3);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.Controls.Add(this.richTextBox1);
+			this.splitContainer2.Panel2.Controls.Add(this.panel2);
+			this.splitContainer2.Size = new System.Drawing.Size(256, 388);
+			this.splitContainer2.SplitterDistance = 172;
+			this.splitContainer2.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(254, 18);
+			this.panel2.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "depends on";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 3);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(47, 12);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "used by";
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.label2);
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel3.Location = new System.Drawing.Point(0, 0);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(254, 18);
+			this.panel3.TabIndex = 2;
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox2.Location = new System.Drawing.Point(0, 18);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(254, 152);
+			this.richTextBox2.TabIndex = 3;
+			this.richTextBox2.Text = "";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Location = new System.Drawing.Point(0, 18);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(254, 192);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 366);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(595, 22);
+			this.statusStrip1.TabIndex = 0;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(855, 413);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.toolStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Dependency Aanalysts [Medcl\']";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+			this.splitContainer2.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
+		private System.Windows.Forms.ToolStripComboBox comboBox1;
+		private System.Windows.Forms.ToolStripButton toolStripButton2;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.SplitContainer splitContainer2;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+	}
+}
+
---
+++ Dependency/Dependency/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.Glee;
+using Microsoft.Glee.Drawing;
+using Microsoft.Glee.GraphViewerGdi;
+using Microsoft.Glee.Splines;
+using Color = System.Drawing.Color;
+using Node = Microsoft.Glee.Drawing.Node;
+
+namespace Dependency
+{
+	public partial class Form1 : Form
+	{
+		private readonly GViewer _viewer;
+
+		private HashSet<string> _references = new HashSet<string>();
+		private string folder;
+
+		public Form1()
+		{
+			InitializeComponent();
+			_viewer = new GViewer();
+			_viewer.BorderStyle = BorderStyle.None;
+
+			_viewer.PanButtonPressed = true;
+
+			_viewer.MouseClick += viewer_MouseClick;
+			_viewer.MouseDoubleClick += viewer_MouseDoubleClick;
+			_viewer.MouseWheel += viewer_MouseWheel;
+			_viewer.OutsideAreaBrush = new Pen(Color.White).Brush;
+			_viewer.Dock = DockStyle.Fill;
+			_viewer.AutoScaleMode = AutoScaleMode.Dpi;
+			panel1.Controls.Add(_viewer);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				folder = folderBrowserDialog1.SelectedPath;
+				_references = new HashSet<string>();
+				comboBox1.Items.Clear();
+				string[] directory = Directory.GetFiles(folder);
+				foreach (string assemblyFile in directory)
+				{
+					if (assemblyFile.ToLower().EndsWith(".dll"))
+					{
+						try
+						{
+							Assembly assembly = Assembly.ReflectionOnlyLoadFrom(assemblyFile);
+							AssemblyName[] references = assembly.GetReferencedAssemblies();
+							string filename = Path.GetFileName(assemblyFile);
+							foreach (AssemblyName file in references)
+							{
+								if (file.Name.ToLower().StartsWith("system") || file.Name.ToLower().StartsWith("mscorlib"))
+								{
+									continue;
+								}
+								_references.Add(string.Format("{0},{1}.dll", filename, file.Name));
+							}
+							comboBox1.Items.Add(filename);
+						}
+						catch (BadImageFormatException)
+						{
+							continue;
+						}
+						catch(FileLoadException)
+						{
+							continue;
+						}catch(Exception)
+						{
+							Debugger.Break();
+						}
+					}
+				}
+			}
+		}
+
+
+		private void viewer_MouseClick(object sender, MouseEventArgs e)
+		{
+		}
+
+
+		private object GetObjectAt(int x, int y)
+		{
+			object result = null;
+			try
+			{
+				result = _viewer.GetObjectAt(x, y);
+			}
+			catch (NullReferenceException)
+			{
+			}
+			return result;
+		}
+
+		private void viewer_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			var node = GetObjectAt(e.X, e.Y) as Node;
+
+			if (node != null) Analyze(node.Id);
+			//more thing here....
+		}
+
+		private void viewer_MouseWheel(object sender, MouseEventArgs e)
+		{
+			if (e.Delta < 0)
+				_viewer.ZoomF *= 1.1;
+			else
+				_viewer.ZoomF *= 0.9;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string dll = comboBox1.Text;
+			Analyze(dll);
+		}
+
+		private void Analyze(string dll)
+		{
+			if (!string.IsNullOrEmpty(dll))
+			{
+				toolStripStatusLabel1.Text = dll;
+				var graph = new Graph("glee");
+				richTextBox1.Clear();
+				richTextBox2.Clear();
+
+				foreach (string relation in _references)
+				{
+					if (relation.StartsWith(dll))
+					{
+						string[] strs = relation.Split(',');
+						graph.AddEdge(strs[0], strs[1]).Attr.Color=Microsoft.Glee.Drawing.Color.DarkBlue;
+						richTextBox1.AppendText(strs[1] + "\n");
+						graph.FindNode(strs[1]).Attr.Fillcolor = Microsoft.Glee.Drawing.Color.Blue;
+					}else if(relation.EndsWith(dll))
+					{
+						string[] strs = relation.Split(',');
+						graph.AddEdge(strs[0], strs[1]).Attr.Color = Microsoft.Glee.Drawing.Color.DarkRed; 
+						richTextBox2.AppendText(strs[0] + "\n");
+						graph.FindNode(strs[0]).Attr.Fillcolor = Microsoft.Glee.Drawing.Color.Red;
+					}
+				}
+				var node=graph.FindNode(dll);
+				if(node !=null)node.Attr.Fillcolor = Microsoft.Glee.Drawing.Color.Green;
+				_viewer.Graph = graph;
+				_viewer.CalculateLayout(graph);
+			}
+		}
+	}
+}
